46d078691b41a8e08fae3972e35e82ad44e21065
Atsushi <>
2019-02-15 15:24:37 +0800

    タスク表示画面作成

--- index.php
+++ index.php
@@ -4,10 +4,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Todoアプリ</title>
+  <title>Todo</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/reset.css">
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-  test
+  <header>
+    <input type="text" id="task" placeholder="タスクを入力してください">
+    <i id="add" class="fas fa-plus-circle fa-2x"></i>
+  </header>
+
+  <main class="container">
+    <!-- 未完了のタスク -->
+    <h2 class="title">Not yet</h2>
+    <ul class="list" id="not-yet"></ul>
+
+    <!-- 完了済みのタスク  -->
+    <h2 class="title">done</h2>
+    <ul class="list" id="done"></ul>
+  </main>
 </body>
 </html>
